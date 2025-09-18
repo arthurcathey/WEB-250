2f91dfcf3a6d9829e3d52db414bfbf640ed55f37
Arthur Cathey <arthurlcathey@students.abtech.edu>
2025-09-18 14:32:19 -0400

    Add asgn04-constructor: Bird class with constructor

--- asgn04-constructors/constructor.php
+++ asgn04-constructors/constructor.php
@@ -1,1 +1,24 @@
 <?php
+class Bird
+{
+  public string $commonName;
+  public string $latinName;
+
+  public function __construct(string $commonName, string $latinName)
+  {
+    $this->commonName = $commonName;
+    $this->latinName = $latinName;
+  }
+
+  public function summary(): string
+  {
+    return "Common name: {$this->commonName}<br>" .
+      "Latin name: {$this->latinName}";
+  }
+}
+
+$bird1 = new Bird("American Robin", "Turdus migratorious");
+$bird2 = new Bird("Eastern Towhee", "Pipilo erythrophthalmus");
+
+echo $bird1->summary() . "<br><hr>";
+echo $bird2->summary() . "<br>";
